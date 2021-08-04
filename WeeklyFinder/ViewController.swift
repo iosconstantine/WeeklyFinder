776b2f46428191e3d26eed1f31775bd833696bf5
Konstantin <maestro.constantine@mail.ru>
2021-08-05 00:06:20 +0300

    This is my second app.
    You can find the day of the week by entering the day, month and year!

---
+++ WeeklyFinder/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  WeeklyFinder
+//
+//  Created by KONSTANTIN TISHCHENKO on 04.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dayTextField: UITextField!
+    @IBOutlet weak var montTextField: UITextField!
+    @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findDay(_ sender: UIButton) {
+        
+        guard let day = dayTextField.text, let month = montTextField.text, let year = yearTextField.text else {
+            return
+        }
+        
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(day)
+        dateComponents.month = Int(month)
+        dateComponents.year = Int(year)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
+        dateFormatter.dateFormat = "EEEE"
+        
+        guard let date = calendar.date(from: dateComponents) else {
+            return
+        }
+        
+        let weekday = dateFormatter.string(from: date)
+        resultLabel.text = weekday.capitalized
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+}
+
